b359f6f43c23188c684f895c50268d7f420c966d
erhaem <jurig.blogger@gmail.com>
2025-01-23 12:17:01 +0700

    feat: add comment

---
+++ comment/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// this is a single line (inline) comment
+	// used to describe something in your code
+	// or to tell the machine to ignore unused line
+	// of code like the variable below
+
+	//unusedVar := "i'm being unused, comment me"
+	// that variable unusedVar will be ignored by the machine
+	// and will never be a thing
+
+	/*
+
+	   If you think the comment would be way tooooo looooong,
+	   and you're sick of typing "//" all over the place,
+	   please consider using this type of comment, called "multiline comment"
+
+	*/
+
+	// this line below will show you a link to where I learn Go
+	fmt.Println("https://dasarpemrogramangolang.novalagung.com/")
+
+}
