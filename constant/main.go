76fa925ae737d03db48201b427bfa1478ebebf42
erhaem <jurig.blogger@gmail.com>
2025-01-27 22:29:56 +0700

    feat: add constant

---
+++ constant/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// below is a constant, once you declared it u can't modify the value
+	const firstName, lastName string = "Agus", "Susanto"
+
+	const schoolName string = "SMKN 666"
+
+	// this wont work:
+	// firstName = "Agung"
+	// once "Agus" always "Agus"!
+
+	// to declare constants, you can also do this:
+	const (
+		age  int = 29
+		city     = "New York" // this one use type inference
+	)
+
+	// here is ugly output
+	fmt.Println(
+		"My name is", firstName+" "+lastName,
+		".", age, "is my age", // does this remind you of something?
+		". I live in ", city,
+		". I go to a boring school daily, "+schoolName,
+	)
+}
