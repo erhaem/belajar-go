6280a5ebbdecac423b49e37c58f07164eab9ab69
erhaem <jurig.blogger@gmail.com>
2025-01-28 12:20:38 +0700

    feat(operator): add negation operator

--- operator/main.go
+++ operator/main.go
@@ -4,14 +4,15 @@ import "fmt"
 
 func main() {
 	multiple := 5 * 3
-	iGotNoIdea := multiple / 3
+	quotient := multiple / 3
 
 	isEven := multiple%2 == 0
-	isGreater := multiple > iGotNoIdea
+	isGreater := multiple > quotient
 
-	fmt.Println(multiple, iGotNoIdea)
-	fmt.Println("is", multiple, "even?", isEven)
-	fmt.Println("is", multiple, "greater than", iGotNoIdea, "?", isGreater)
+	fmt.Println(multiple, quotient)
+	fmt.Printf("is %d even? %t\n", multiple, isEven)
+	fmt.Printf("is %d greater than %d? %t\n", multiple, quotient, isGreater)
 
-	// this quite boring. i quit
+	fmt.Printf("it is %t that %d is not greater than %d\n", !isGreater, multiple, quotient)
+	// 														^^^^^^^^^^ not-logical operator (negation)
 }
